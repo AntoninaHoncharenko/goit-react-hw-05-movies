fba97289e0cc8a9df8a1bfc28e0af54c59f5fc7c
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2023-01-05 23:39:48 +0200

    add fetch movie by name

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,8 +1,17 @@
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (event.currentTarget.elements.query.value === '') {
+      event.currentTarget.reset();
+      return;
+    }
+    onSubmit(event.currentTarget.elements.query.value);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" />
+      <form onSubmit={event => handleSubmit(event)}>
+        <input type="text" name="query" />
         <button type="submit">Search</button>
       </form>
     </div>
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react';
 import { SearchForm } from 'components/SearchForm/SearchForm';
+import { fetchMoviesByName } from '../../api';
+import { MovieList } from 'components/MovieList/MovieList';
 
 export const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [name, setName] = useState('');
+
+  const handleSubmit = query => {
+    setName(query);
+  };
+
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        const data = await fetchMoviesByName(name);
+        setMovies(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMovie();
+  }, [name]);
+
   return (
     <div>
-      <SearchForm />
+      <SearchForm onSubmit={handleSubmit} />
+      {movies && <MovieList movies={movies} />}
     </div>
   );
 };
